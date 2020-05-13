1c4ae8274164cbdb6daf3362301438ca5404c19c
Alexandr Olimpiev <alexandr.olimpiev@gmail.com>
2020-05-13 16:48:21 +0300

    Учебный проект: революция или эволюция? (Часть 1)

--- src/components/task-edit.js
+++ src/components/task-edit.js
@@ -3,7 +3,9 @@ import {
   DAYS,
   MONTH_NAMES
 } from "../const.js";
+
 import {
+  createElement,
   formatTime
 } from "../utils.js";
 
@@ -52,7 +54,7 @@ const createColorsMarkup = (colors, currentColor) => {
 };
 
 
-export const createTaskEditTemplate = (task) => {
+const createTaskEditTemplate = (task) => {
   const {
     description,
     dueDate,
@@ -99,8 +101,7 @@ export const createTaskEditTemplate = (task) => {
                         <button class="card__date-deadline-toggle" type="button">
                           date: <span class="card__date-status">${isDateShowing ? `yes` : `no`}</span>
                         </button>
-                        ${isDateShowing ?
-      `<fieldset class="card__date-deadline">
+                        ${isDateShowing ? `<fieldset class="card__date-deadline">
                           <label class="card__input-deadline-wrap">
                             <input
                               class="card__date"
@@ -110,29 +111,23 @@ export const createTaskEditTemplate = (task) => {
                               value="${date} ${time}"
                             />
                           </label>
-                        </fieldset>`
-      : ``
-    }
+                        </fieldset>` : ``}
 
                         <button class="card__repeat-toggle" type="button">
                           repeat:<span class="card__repeat-status">${isRepeatingTask ? `yes` : `no`}</span>
                         </button>
-                        ${
-    isRepeatingTask ?
-      `<fieldset class="card__repeat-days">
+                        ${isRepeatingTask ? `<fieldset class="card__repeat-days">
                           <div class="card__repeat-days-inner">
                             ${repeatingDaysMarkup}
                           </div>
-                        </fieldset>`
-      : ``
-    }
+                        </fieldset>` : ``}
                       </div>
-                    </div>
 
-                    <div class="card__colors-inner">
-                      <h3 class="card__colors-title">Color</h3>
-                      <div class="card__colors-wrap">
-                        ${colorsMarkup}
+                      <div class="card__colors-inner">
+                        <h3 class="card__colors-title">Color</h3>
+                        <div class="card__colors-wrap">
+                          ${colorsMarkup}
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -146,3 +141,26 @@ export const createTaskEditTemplate = (task) => {
     </article>`
   );
 };
+
+export default class TaskEdit {
+  constructor(task) {
+    this._task = task;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTaskEditTemplate(this._task);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
--- src/components/tasks.js
+++ src/components/tasks.js
@@ -2,19 +2,20 @@ import {
   createElement
 } from "../utils.js";
 
-const createLoadMoreButtonTemplate = () => {
+const createTasksTemplate = () => {
   return (
-    `<button class="load-more" type="button">load more</button>`
+    `<div class="board__tasks"></div>`
   );
 };
 
-export default class LoadMoreButton {
+
+export default class Tasks {
   constructor() {
     this._element = null;
   }
 
   getTemplate() {
-    return createLoadMoreButtonTemplate();
+    return createTasksTemplate();
   }
 
   getElement() {
